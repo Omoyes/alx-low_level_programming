ba3e67a3fda32044ef333e7d0195d8a582f07992
Omoyes <glowriaogar@gmail.com>
2022-04-07 20:09:36 -0700

    commit four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
-/**
-*Description: main - prints out a string stdout
-*
-*Return: 0 if success
-*/
-int main(void)
-{
-puts("/"Programming is like building a multilingual puzzle");
-Return (0);
-{
+#include <stdio.h> 
+
+/** 
+
+* Description: main - Prints out a string to stdout. 
+	
+* Return: 0 if success. 
+
+*/ 
+
+int main(void) 
+
+{ 
+
+put("\"Programming is like building a multilingual puzzle"); 
+
+return(0); 
+ 
+}
