662da32a81b3f5e1deb1ceb777fb6893f57ebf53
Omoyes <glowriaogar@gmail.com>
2022-04-07 20:23:53 -0700

    commit five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,19 @@
-#include <stdio.h> 
+#​include​ ​<​stdio.h​> 
 
-/** 
+ ​/*​* 
 
-* Description: main - Prints out a string to stdout. 
+ ​ * Description: main - Prints out a string to stdout. 
 
-* Return: 0 if success. 
+ ​ * Return: 0 if success. 
 
-*/ 
+ ​ ​*/ 
 
-int main(void) 
+ ​int​ ​main​(​void​) 
 
-{ 
+ ​{ 
 
-printf("with proper grammar, but the outcome is a piece of art,\n"); 
+ ​printf​(​"​with proper grammar, but the outcome is a piece of art,​\n​"​); 
 
-return (0); 
+ ​return​ (​0​); 
 
-}
+ ​}
